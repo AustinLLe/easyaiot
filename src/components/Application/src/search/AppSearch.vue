7675cef8a3f75928ae2f1ef758ef334ff36372c5
Kang <Austin.g.lee@outlook.com>
2026-08-15 18:53:06 +0800

    t

--- src/components/Application/src/search/AppSearch.vue
+++ src/components/Application/src/search/AppSearch.vue
@@ -4,7 +4,7 @@ import { Tooltip } from 'ant-design-vue'
 import { SearchOutlined } from '@ant-design/icons-vue'
 import AppSearchModal from './AppSearchModal.vue'
 import { useI18n } from '@/hooks/web/useI18n'
-
+//1
 export default defineComponent({
   name: 'AppSearch',
   setup() {
